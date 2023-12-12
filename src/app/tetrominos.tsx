a3e2f1c18f94819d07b35aed9e39a108347dd730
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-12 19:53:05 +0000

    defined tetrominos object

---
+++ src/app/tetrominos.tsx
@@ -0,0 +1,10 @@
+export const tetrominos = {
+    0: {shape: [[0]], color: "color-1"},
+    I: {shape: [[0]], color: "color- 2"},
+    L: {shape: [[0]], color: "color- 3"},
+    J: {shape: [[0]], color: "color- 4"},
+    O: {shape: [[0]], color: "color- 5"},
+    S: {shape: [[0]], color: "color- 6"},
+    Z: {shape: [[0]], color: "color- 7"},
+    T: {shape: [[0]], color: "color- 8"},
+8
