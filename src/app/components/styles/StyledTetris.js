fbc473f85b501d06edce184d9d8a3f3dcb87ddd3
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-15 18:21:14 +0000

    changed main Tetris wrapper to a flex box

--- src/app/components/styles/StyledTetris.js
+++ src/app/components/styles/StyledTetris.js
@@ -1,13 +1,7 @@
 import styled from "styled-components";
 
 export const StyledTetris = styled.div`
-  --grid-width: calc((var(--square-size) * var(--cols)));
-  --grid-height: calc((var(--square-size) * var(--rows)));
-
-  display: grid;
-  grid-gap: var(--square-size);
-  grid-template-columns: 100px var(--grid-width) 100px;
-  grid-template-rows: 100px var(--grid-height) 100px;
-
+  display: flex;
+  gap: 10px;
   justify-content: center;
 `;
