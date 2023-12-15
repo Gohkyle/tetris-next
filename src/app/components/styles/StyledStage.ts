aba1ad5dc23e0052820c85473b8f60897739a6c6
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-15 22:45:54 +0000

    reverted to fixed pixels

--- src/app/components/styles/StyledStage.ts
+++ src/app/components/styles/StyledStage.ts
@@ -2,11 +2,7 @@ import styled from "styled-components";
 
 export const StyledStage = styled.div<{height:number, width:number}>`
   display: grid;
-
-  width:25vw;
-  max-width:1000px;
-  grid-template-rows: repeat(${(props)=>props.height}, calc(25vw /${(props)=>props.width}));
-  grid-template-columns: repeat(${(props)=>props.width},  1fr);
-  grid-gap:1px;
+  grid-template-columns: repeat(${(props)=>props.width}, var(--square-size));
+  grid-template-rows: repeat(${(props)=>props.height}, var(--square-size));
   align-self: flex-start;
 `;
