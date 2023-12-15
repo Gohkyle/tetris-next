9a988da14426ce52609917f6ef913291a88459f4
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-15 19:26:19 +0000

    organised code added bg coor

--- src/app/components/styles/StyledNextBlock.ts
+++ src/app/components/styles/StyledNextBlock.ts
@@ -5,9 +5,11 @@ export const StyledNextBlock = styled.div<{size: number}>`
   width: calc(4 *var(--square-size));
   height: calc(4 *var(--square-size));
 
-  place-content: center;
   grid-template-columns: repeat(${props=>props.size}, var(--square-size));
   grid-template-rows: repeat(${props=>props.size}, var(--square-size));
+  place-content: center;
   grid-gap: 0;
-  align-self: flex-start;
+
+
+  background-color: var(--color-0);
 `;
