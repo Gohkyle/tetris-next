8e9f0ed933b1119ef6e3ad4a6796c70099ef6e23
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-15 19:54:36 +0000

    removed unecesssary scoreboard components

--- src/app/components/Tetris.tsx
+++ src/app/components/Tetris.tsx
@@ -1,17 +1,19 @@
 import { Stage } from './Stage'
 import { NextBlock } from './NextBlock'
-import { ScoreBoard } from './ScoreBoard'
 import { MessageModal } from './MessageModal'
 import { StyledTetris } from './styles/StyledTetris'
 import { createStage } from '../gameHelper'
+import { Display } from './Display'
+
 
 export const Tetris = () => {
     return (
         <StyledTetris>
             <Stage stage={createStage()}/>
             <aside>
-            <NextBlock nextBlock={[["O","O"],["O", "O"]]} />
-            <ScoreBoard/>
+                <NextBlock nextBlock={[["0", "0" ,"0"],["0", "O", "0"], ["O", "O", "O"]]} />
+                <Display text="score"/>
+                <Display text="level"/>
             </aside>
             <MessageModal title="title" text="text"/>
         </StyledTetris>
