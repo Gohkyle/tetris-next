e85e506889565fbf881cf35263d1e4156c431bc6
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-16 18:40:20 +0000

    interface Iprops renamed

--- src/app/components/Stage.tsx
+++ src/app/components/Stage.tsx
@@ -2,11 +2,11 @@ import { Square } from './Square'
 import { StyledStage } from './styles/StyledStage'
 import { Shape } from '../types'
 
-interface Stage{
-    stage: (Shape)[][]
+interface IProps{
+    stage: Shape[][]
 }
 
-export const Stage = ({stage}:Stage) => {
+export const Stage = ({stage}:IProps) => {
     return (
         <StyledStage height={stage.length} width={stage[0].length}>
             {stage.map((line: Shape [], row: number)=>{
