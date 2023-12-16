55c888cbd275fbb04fc5d74c7239bf71f1f194d6
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-16 18:41:41 +0000

    removed event handler to tetris component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-"use client"
-
 import { Tetris } from './components/Tetris'
 
-interface iProps {
-  key: string
-}
-
 export default function Home() {
-  const handleButtonPress = ({key}: iProps) => {
-    console.log(key)
-}
   return (
-    <main tabIndex={0} onKeyUp={handleButtonPress}>
+    <main>
       <Tetris/>
     </main>
   )
