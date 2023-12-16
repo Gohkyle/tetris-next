ac0132ca0fc5b268ce9d33c6c8457543cdf543f3
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-16 18:39:36 +0000

    rearranged

--- src/app/gameActions.tsx
+++ src/app/gameActions.tsx
@@ -1,9 +1,9 @@
-export const createStage = () => {}
 export const pauseGame = () => {}
 export const resumeGame = () => {}
+export const restartGame = () => {}
+
 export const moveLeft = () => {}
 export const moveRight = () => {}
 export const moveDown = () => {}
 export const rotate = () => {}
 export const drop = () => {}
-export const restartGame = () => {}
