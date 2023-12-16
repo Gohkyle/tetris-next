fc1f1b5ccb540c917c92fc5b75b830c80be9f4b8
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-16 01:37:24 +0000

    refactored and added display with styled components

---
+++ src/app/components/styles/StyledDisplay.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledDisplay = styled.div`
+  border: 5px solid grey;
+  border-radius: 5px;
+  margin-top: 1em;
+  background-color: var(--color-0);
+  color:white;
+  font-family: PressStart2P;
+  font-size:0.8rem;
+    padding: 1em;
+`;
