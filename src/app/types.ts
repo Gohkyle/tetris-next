1a508e7eb9ddcb924eb573a76e922b802f10efcc
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-19 22:18:57 +0000

    Player interface and Position interface defined

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,1 +1,12 @@
 export type Shape = 0 | "I" | "L" | "J" | "O" | "T" | "S" | "Z";
+
+export interface Player{
+    currTetro: Shape[][],
+    position: Position
+    hasCollided: boolean
+}
+
+export interface Position{
+    x: number,
+    y: number
+}
