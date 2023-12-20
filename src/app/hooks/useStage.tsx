c9de4895c67be78f6cd7bfbc24dba521cb5582c8
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-20 15:26:22 +0000

    useEffect to monitor stage changes and rerenders

--- src/app/hooks/useStage.tsx
+++ src/app/hooks/useStage.tsx
@@ -2,33 +2,34 @@ import { useState, useEffect } from "react";
 
 import { createStage } from "../gameHelper";
 
-import { Shape, Player} from "../types";
+import { Shape, Player } from "../types";
 
+export const useStage = (player: Player) => {
+  const [stage, setStage] = useState(createStage());
 
-export const useStage = (player: Player
-    ) => {
-    const [stage, setStage] = useState(createStage())
+  useEffect(() => {
+    const updateStage = (prevStage: Shape[][]) => {
+      const newStage = prevStage.map((row) => {
+        return row.map((square: Shape): Shape => {
+          return 0;
+        });
+      });
 
-    useEffect(()=>{
-        const updateStage = (prevStage: Shape[][]) => {
-            const newStage = prevStage.map((row)=>{
-                return row.map((square: Shape): Shape=>{
-                    return 0;
-                })
-            })
+      player.currTetro.forEach((row: Shape[], y: number) => {
+        return row.forEach((square: Shape, x: number) => {
+          if (square !== 0) {
+            return (newStage[y + player.position.y][x + player.position.x] =
+              square);
+          }
+        });
+      });
 
-            player.currTetro.forEach((row:Shape[], y:number)=>{
-                return row.forEach((square:Shape, x:number)=>{
-                    if (square !== 0) {
-                        return newStage[y+player.position.y][x+player.position.x] = square
-                    }
-                })
-            })   
-        return newStage;
-        }
+      return newStage;
+    };
 
-        setStage((prevStage) => updateStage(prevStage))
-    }, [player])
+    setStage((prevStage) => updateStage(prevStage));
 
-    return [stage, setStage] as const;
-}
+  }, [player]);
+
+  return [stage, setStage] as const;
+};
