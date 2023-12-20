4cc066b5bb2655bbd43911210a72e89f8cf71d89
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-20 15:25:14 +0000

    player state, updatePlayerPos and resetPlayer defined

---
+++ src/app/hooks/usePlayer.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useState } from "react";
+import { columns } from "../gameHelper";
+import { randomTetromino } from "../tetrominos";
+import { Player } from "../types";
+
+interface IProps{
+    x: number,
+    y:number,
+    hasCollided: boolean
+}
+
+export const usePlayer = () => {
+    const [player, setPlayer] = useState<Player>({
+        currTetro:[[0]],
+        position: {x:0, y:0},
+        hasCollided: false
+    })
+    
+    const updatePlayerPos = ({x, y, hasCollided}: IProps) => {
+        setPlayer((prevPlayer)=>{
+            return {
+                ...prevPlayer,
+                position: {x: prevPlayer.position.x + x, y: prevPlayer.position.y + y},
+                hasCollided
+            }
+        })
+    }
+
+    const resetPlayer = useCallback(():void=>{
+        const nextBlock = randomTetromino();
+        console.log(nextBlock, "random tetromino from resetPlayer")
+        setPlayer({
+            currTetro: nextBlock,
+            position:{x: columns/2 - Math.floor(nextBlock.length/2), y: 0},
+            hasCollided: false
+        })
+    }, [])
+    
+    return [player, updatePlayerPos, resetPlayer] as const
+}
