130bc184f5c8b1154ab738d6f6a260bbdaad099b
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-20 15:23:03 +0000

    implemented movement with keys

--- src/app/components/Tetris.tsx
+++ src/app/components/Tetris.tsx
@@ -1,63 +1,60 @@
 "use client";
 
+import { StyledTetris } from "./styles/StyledTetris";
+
 import { Stage } from "./Stage";
 import { NextBlock } from "./NextBlock";
 import { MessageModal } from "./MessageModal";
-import { StyledTetris } from "./styles/StyledTetris";
-import { createStage } from "../gameHelper";
 import { Display } from "./Display";
 
-import { randomTetromino } from "../tetrominos";
+import { useStage } from "../hooks/useStage";
+import { usePlayer } from "../hooks/usePlayer";
+
 import { useState } from "react";
+
+import { Shape, Player } from "../types";
 
 interface IProps {
   keyCode: number;
 }
 
 export const Tetris = () => {
+  const [player, updatePlayerPos] = usePlayer();
+  const [stage, setStage] = useStage(player);
+  const [nextBlock, setNextBlock] = useState<Shape[][]>([[0]]);
+
+  // const [isGameOver, setIsGameOver] = useState(false);
+
   const handleButtonPress = ({ keyCode }: IProps) => {
-    console.log(keyCode)
     // if(keyCode===27){
 
     // }
-    // if (keyCode===37){
-    //     movePlayer(-1);
-    // }
+    if (keyCode === 37) {
+      updatePlayerPos({ x: -1, y: 0, hasCollided: false });
+    }
     // if (keyCode===38){
-    //     rotatePlayer()
+    //   rotatePlayer()
     // }
-    // if (keyCode===39){
-    //     movePlayer(1)
-    // }
-    // if (keyCode===40){
-    //     drop()
-    // }
-    // if (keyCode===32){
-    //     dropPlayer()
+    if (keyCode === 39) {
+      updatePlayerPos({ x: 1, y: 0, hasCollided: false });
+    }
+    if (keyCode === 40) {
+      updatePlayerPos({ x: 0, y: 1, hasCollided: false });
+    }
+    // if (keyCode === 32) {
+    //   dropPlayer();
     // }
   };
 
-  const [nextBlock, setNextBlock] = useState([[0]]);
-  const [player, setPlayer] = useState({
-    currTetro: [[0]],
-    position: { x: 0, y: 0 },
-    hasCollided: false,
-  });
-  const [stage, setStage] = useState(createStage());
-
-  const [isGameOver, setIsGameOver] = useState(false);
-
-  const startGame = () => {
-  };
   return (
-    <StyledTetris tabIndex={0} onKeyUp={handleButtonPress}>
+    <StyledTetris role= "button" tabIndex={0} onKeyUp={handleButtonPress}>
       <Stage stage={stage} />
       <aside>
         <NextBlock nextBlock={nextBlock} />
         <Display text="score" />
         <Display text="level" />
       </aside>
-      <MessageModal title="title" text="text"/>
+      <MessageModal title="title" text="text" />
     </StyledTetris>
   );
 };
