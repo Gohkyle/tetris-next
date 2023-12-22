86bf4d0979dbbba96897aed08618b6ab65af03fc
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-22 16:28:07 +0000

    extracted updateStage() as a function to be tested

--- src/app/hooks/useStage.tsx
+++ src/app/hooks/useStage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { createStage } from "../utils/gameHelper";
+import { createStage, updateStage} from "../utils/gameHelper";
 
 import { Shape, Player } from "../types";
 
@@ -8,28 +8,7 @@ export const useStage = (player: Player) => {
   const [stage, setStage] = useState(createStage());
 
   useEffect(() => {
-
-    const updateStage = (prevStage: Shape[][]) => {
-      const newStage = prevStage.map((row) => {
-        return row.map((square: Shape): Shape => {
-          return 0;
-        });
-      });
-
-      player.currTetro.forEach((row: Shape[], y: number) => {
-        return row.forEach((square: Shape, x: number) => {
-          if (square !== 0) {
-            return (newStage[y + player.position.y][x + player.position.x] =
-              square);
-          }
-        });
-      });
-
-      return newStage;
-    };
-
-    setStage((prevStage:Shape[][]) : Shape [][]=> updateStage(prevStage));
-console.log( "useEffect rendering")
+    setStage((prevStage:Shape[][]) : Shape [][]=> updateStage(prevStage, player));
   }, [player]);
 
   return [stage, setStage] as const;
