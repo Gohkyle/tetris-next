cfed242c4586eed56fecee0fa865c1efa29b50c6
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2023-12-23 02:54:38 +0000

    resetPlayer when collision detected

--- src/app/hooks/useStage.tsx
+++ src/app/hooks/useStage.tsx
@@ -4,12 +4,17 @@ import { createStage, updateStage} from "../utils/gameHelper";
 
 import { Player, SquareObject } from "../types";
 
-export const useStage = (player: Player) => {
+export const useStage = (player: Player, resetPlayer: ()=>void) => {
   const [stage, setStage] = useState(createStage());
 
   useEffect(() => {
     setStage((prevStage:SquareObject[][]) : SquareObject [][]=> updateStage(prevStage, player));
-  }, [player]);
+
+    if (player.hasCollided){
+      resetPlayer()
+    } 
+      
+  }, [player,resetPlayer]);
 
   return [stage, setStage] as const;
 };
