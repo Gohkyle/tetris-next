b0154754335eca6b86ece67831844f53add6cc0f
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-23 19:45:25 +0000

    added if statements to prevent rotation from breaking stage

--- src/app/hooks/usePlayer.tsx
+++ src/app/hooks/usePlayer.tsx
@@ -1,32 +1,59 @@
 import { useCallback, useState } from "react";
-import { columns, rotate, updatePlayerPos } from "../utils/gameHelper";
+import {
+  checkCollision,
+  columns,
+  rotate,
+  updatePlayerPos,
+} from "../utils/gameHelper";
 import { randomTetromino } from "../tetrominos";
-import { Movement, Player } from "../types";
+import { Movement, Player, SquareObject } from "../types";
 
 export const usePlayer = () => {
-    const [player, setPlayer] = useState<Player>({
-        currTetro:[[0]],
-        position: {x:0, y:0},
-        hasCollided: false
-    })
-    
-    const updatePlayer = (movement: Movement) => {
-        setPlayer((prevPlayer)=> updatePlayerPos(prevPlayer, movement))
-    }
+  const [player, setPlayer] = useState<Player>({
+    currTetro: [[0]],
+    position: { x: 0, y: 0 },
+    hasCollided: false,
+  });
 
-    const rotatePlayer = () => {
+  const updatePlayer = (movement: Movement) => {
+    setPlayer((prevPlayer) => updatePlayerPos(prevPlayer, movement));
+  };
+
+  const rotatePlayer = (stage: SquareObject[][]) => {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.currTetro = rotate(clonedPlayer.currTetro);
+
+    if (!checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
         setPlayer((prevPlayer) => {
-            return {...prevPlayer, currTetro: rotate(prevPlayer.currTetro)}})
+            return { ...prevPlayer, currTetro: rotate(prevPlayer.currTetro) };
+        });
+    }else if (!checkCollision(clonedPlayer, stage, { x: 1, y: 0 })) {
+      setPlayer((prevPlayer) => {
+        return { ...prevPlayer, currTetro: rotate(prevPlayer.currTetro), position : {x:player.position.x +1, y: player.position.y} };
+      });
+    } else if (!checkCollision(clonedPlayer, stage, { x: -1, y: 0 })) {
+      setPlayer((prevPlayer) => {
+        return { ...prevPlayer, currTetro: rotate(prevPlayer.currTetro), position : {x:player.position.x -1, y: player.position.y}  };
+      });
+    }else if (!checkCollision(clonedPlayer, stage, { x: 2, y: 0 })) {
+      setPlayer((prevPlayer) => {
+        return { ...prevPlayer, currTetro: rotate(prevPlayer.currTetro), position : {x:player.position.x +2, y: player.position.y} };
+      });
+    } else if (!checkCollision(clonedPlayer, stage, { x: -2, y: 0 })) {
+      setPlayer((prevPlayer) => {
+        return { ...prevPlayer, currTetro: rotate(prevPlayer.currTetro), position : {x:player.position.x -2, y: player.position.y}  };
+      });
     }
+  };
 
-    const resetPlayer = useCallback(()=>{
-        const nextBlock = randomTetromino();
-        setPlayer({
-            currTetro: nextBlock,
-            position:{x: columns/2 - Math.floor(nextBlock.length/2), y: 0},
-            hasCollided: false
-        })
-    }, [])
-    
-    return [player, updatePlayer, resetPlayer, rotatePlayer] as const
-}
+  const resetPlayer = useCallback(() => {
+    const nextBlock = randomTetromino();
+    setPlayer({
+      currTetro: nextBlock,
+      position: { x: columns / 2 - Math.floor(nextBlock.length / 2), y: 0 },
+      hasCollided: false,
+    });
+  }, []);
+
+  return [player, updatePlayer, resetPlayer, rotatePlayer] as const;
+};
