5da00e7d1a1a83fa7f0e5d48d4e1b1b4ed1bf51d
Kyle Goh <KyleGoh@Kyles-MBP-383.lan>
2024-01-24 18:22:53 +0000

    refined gameover logic

--- src/app/hooks/useStatus.tsx
+++ src/app/hooks/useStatus.tsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState, useEffect} from "react";
 
-const [level, setLevel] = useState<number>(0)
-const [score, setScore] = useState<number>(0)
-const [rowsCleared, setRowsCleared] = useState<number>(0)
 
-const useStatus = () => {
-    return [level, score, rowsCleared]
+export const useStatus = (isGameOver:boolean, rowsCleared:number) => {
+    const [level, setLevel] = useState<number>(0)
+    const [score, setScore] = useState<number>(0)
+    const [totalRows, setRows] = useState<number>(0)
+    const [combo, setCombo] = useState<number>(0)
+
+    const lineScore =[100, 300, 500, 800]
+
+    const calcScore = useCallback(()=>{
+        if(!isGameOver){
+            setScore((prevScore)=>{
+                return prevScore += (level+1) * (lineScore[rowsCleared -1])
+            })
+        }
+
+    },[])
+
+    useEffect(()=>{
+        calcScore()
+    },[rowsCleared, calcScore, score])
+    return [level, score, totalRows, combo, calcScore] as const;
 }
